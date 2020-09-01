d117d0e33b7d69aadaec28100644bcf0a651548c
Kostia Netot <pugjazz@gmail.com>
2020-09-01 13:30:36 +0300

    Some chamhes after summer

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-
-gulp.task('sass', function(){
-  return gulp.src('./fal_v2/layout/sass/main.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'));
-});
-
